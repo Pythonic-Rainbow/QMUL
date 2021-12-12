f62b64f0150e17e606a08aa8f6e90887d27a5ca3
Pythonic-Rainbow <wongchingho0@gmail.com>
2021-12-12 22:25:57 +0000

    Level 4 miniproj

--- ECS401/Mini Projects/src/SecretTrapsAndPassages.java
+++ ECS401/Mini Projects/src/SecretTrapsAndPassages.java
@@ -8,43 +8,74 @@ import java.util.Scanner;
  * A board game with traps and secret passages
  */
 public class SecretTrapsAndPassages {
+    record Question(String question, String[] choices, char answer) {}
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final Random random = new Random();
-        final char answer = 'b';
+        final Question question = new Question("What is 1+1?", new String[]{"1", "2", "3", "4"}, 'b');
+        final int[] positions = new int[]{0, 0};
 
-        int position = rollDice(random);
-        System.out.println("Your current position is: " + position);
-        char input = getMCInput("What is 1+1?", new String[]{"1", "2", "3", "4"}, scanner);
-        printInput(input);
-        printAnswer(answer);
+        for (int i = 0; i < positions.length; i++) {
+            System.out.println("Current player: " + (i+1));
+            int roll = rollDice(random);
+            positions[i] += roll;
+            System.out.printf("You rolled %d. Your current position is: %s%n", roll, positions[i]);
 
-        boolean isCorrect = input == answer;
-        position = isCorrect ? position : position - 1;
-        printIsCorrect(isCorrect);
+            // Actual gameplay
+            boolean isCorrect = getMCInput(question, scanner);
+            positions[i] = getNewPosition(positions[i], isCorrect);
+            System.out.printf("Your new position is: %s. Press ENTER to confirm.%n", positions[i]);
+            scanner.nextLine();  // Lets the player check the results before next player
+            System.out.println("====================");
+        }
     }
 
-    // Asks question and returns user choice
-    public static char getMCInput(String q, String[] choices, Scanner s) {
-        System.out.println(q + "\n");
-        for (int i = 0; i <= 3; i++) System.out.printf("%c. %s%n", (char)(i + 97), choices[i]);
-        return s.nextLine().charAt(0);
-    }
-
-    // prints input from user
-    public static void printInput(char input) {
+    // Asks question and returns whether the answer is correct
+    public static boolean getMCInput(Question q, Scanner s) {
+        System.out.println(q.question() + "\n");
+        for (int i = 0; i <= 3; i++) System.out.printf("%c. %s%n", (char)(i + 97), q.choices()[i]);
+        char input = s.nextLine().charAt(0);
         System.out.println("You answered " + input);
-    }
-
-    // prints correct answer
-    public static void printAnswer(char answer) {
-        System.out.println("The correct answer is " + answer);
-    }
-
-    // prints whether the user has inputted correct answer
-    public static void printIsCorrect(boolean correct) {
+        System.out.println("The correct answer is " + q.answer());
+        boolean correct = input == q.answer();
         System.out.printf("Your answer is %s\n", correct ? "correct" : "wrong");
+        return correct;
+    }
+
+    /**
+     * Processes the position of the player
+     * @param pos Current position to be processed
+     * @param correct Whether the player has correctly answered the previous question
+     * @return New position
+     */
+    public static int getNewPosition(int pos, boolean correct) {
+        // Special board positions with traps, secret paths etc.
+        final int[] specialPos = new int[]{3, 5};
+        final String[] posInst = new String[]{"?4", "!4"};
+
+        if (!correct) pos -= 1;  // Go back by 1 if wrong answer
+
+        // Check whether current position is in the special positions array
+        for (int i = 0; i < specialPos.length; i++) if (pos == specialPos[i]) {
+            // Extracts the operand from the instruction corresponding to the special position
+            String operand = posInst[i].substring(1);
+            // The first char of the instruction is the actual command
+            switch (posInst[i].charAt(0)) {
+                case '?':  // If the answer is correct
+                    if (correct)  {
+                        System.out.println("You found a secret path! Jump forward by " + operand);
+                        return pos + Integer.parseInt(operand);  // Increments position by operand
+                    }
+                    break;
+                case '!':  // If the answer is wrong
+                    if (!correct)  {
+                        System.out.println("You fell into a trap! Jump backwards by " + operand);
+                        return pos - Integer.parseInt(operand);  // Decrements position by operand
+                    }
+            }
+        }
+        return pos; // If current position is not special, just return
     }
 
     // returns an integer from 1 to 6 inclusive.
