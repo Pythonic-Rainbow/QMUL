4eb9c1f5f8b23853c279a546c3c1cd953f5d42ed
Pythonic-Rainbow <wongchingho0@gmail.com>
2021-12-13 23:23:18 +0000

    Updates Short 5

--- ECS401/Short Assessed Exercises/src/SAX5.java
+++ ECS401/Short Assessed Exercises/src/SAX5.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * @author Howard Wong
+ * Date: 13-12-2021
+ * @version 1.1
+ * Find the average and all information of olympians
+ */
 public class SAX5 {
-    record Olympian(
-            String name,
-            byte medals,
-            String category
-    ) {}
+    static class Olympian {
+        String name;
+        byte medals;
+        String category;
+    }
 
     public static void main(String[] args) {
         final byte arraySize = 5;
@@ -13,6 +19,15 @@ public class SAX5 {
         askOlympians(arraySize, scanner);
     }
 
+    public static Olympian newOlympian(String name, byte medals, String cat) {
+        Olympian o = new Olympian();
+        o.name = name;
+        o.medals = medals;
+        o.category = cat;
+        return o;
+    }
+
+    // Obtains all the info of all olympian from the user
     public static void askOlympians(byte size, Scanner s) {
         Olympian[] olympians = new Olympian[size];
         float sum = 0;
@@ -26,11 +41,12 @@ public class SAX5 {
             System.out.print("What sport did he/she compete in? ");
             String category = s.nextLine();
             System.out.println();
-            olympians[i] = new Olympian(name, medals, category);
+            olympians[i] = newOlympian(name, medals, category);
         }
         printOlympiansInfo(olympians, sum);
     }
 
+    // Prints all info for all olympians
     public static void printOlympiansInfo(Olympian[] olympians, float sum) {
         System.out.printf("Between them they won an average of %d medals each.%n", Math.round(sum / olympians.length));
         for (Olympian o : olympians) System.out.printf("%s, %s, %d%n", o.category, o.name, o.medals);
