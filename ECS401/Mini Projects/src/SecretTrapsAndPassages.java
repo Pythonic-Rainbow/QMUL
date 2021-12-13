f67badc115587549acf746ea691b2f44124b2161
Pythonic-Rainbow <wongchingho0@gmail.com>
2021-12-13 21:08:49 +0000

    Level 7 for miniproj

--- ECS401/Mini Projects/src/SecretTrapsAndPassages.java
+++ ECS401/Mini Projects/src/SecretTrapsAndPassages.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /**
  * @author Howard Wong
  * Date: 12-12-2021
- * @version 5.0
+ * @version 7.0
  * A board game with traps and secret passages
  */
 public class SecretTrapsAndPassages {
@@ -42,29 +42,93 @@ public class SecretTrapsAndPassages {
         return mc;
     }
 
+    // Simulating how arrays are implemented through a stack (ADT)
+    static class QuestionBank {
+        static final Question ONE = newQuestion("What is 1+1?", newMC("1", "2", "3", "4"), 'b');
+        static final Question TWO = newQuestion("What is the nuclear reaction type of the Sun?",
+                newMC("Fusion", "Fission", "Friction", "Tension"), 'a');
+        static final Question THREE = newQuestion("Where is Queen Mary, University of London located at?",
+                newMC("Queen", "Mary", "University", "London"), 'd');
+        static final Question FOUR = newQuestion("In Chinese, both '您' and '你' means 'you', but when should we use 您 over 你?",
+                newMC("When that person has a heart", "When you respect that person",
+                        "When we are praying", "When that person gets COVID-19"), 'b');
+        static final Question FIVE = newQuestion("Who directed Star Wars Ep. 7 and 9?",
+                newMC("Jar Jar Binks", "Luke Starkiller", "J. J. Abrams", "George Lucas"), 'c');
+        static final Question SIX = newQuestion("How do you pronounce で(De)す(Su) in spoken Japanese?",
+                newMC("De su", "BAN ZAI", "D esu", "Des"), 'd');
+    }
+
+    // Simulating how we access elements in a stack by traversing through it linearly until we found a match
+    public static Question getRandomQuestion(Random r) {
+        switch (rollDice(r)) {
+            case 1:
+                return QuestionBank.ONE;
+            case 2:
+                return QuestionBank.TWO;
+            case 3:
+                return QuestionBank.THREE;
+            case 4:
+                return QuestionBank.FOUR;
+            case 5:
+                return QuestionBank.FIVE;
+            default:
+                return QuestionBank.SIX;
+        }
+    }
+
+    // Bubble sort
+    public static int[] sortedPositions(int[] poss) {
+        int temp;
+        for (int j = 0; j < poss.length; j++) {
+            for (int i = j; i < poss.length - 1; i++) {
+                if (poss[i] < poss[i+1]) {
+                    temp = poss[i];
+                    poss[i] = poss[i+1];
+                    poss[i+1] = temp;
+                }
+            }
+        }
+        return poss;
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final Random random = new Random();
-        final Question question = newQuestion("What is 1+1?", newMC("1", "2", "3", "4"), 'b');
-        final int[] positions = new int[2];
         // Special board positions with traps, secret paths etc.
         final int[] specialPos = new int[]{3, 5};
-
         final String[] posInst = new String[]{"?4", "!4"};
 
-        for (int i = 0; i < positions.length; i++) {
-            System.out.println("Current player: " + (i+1));
-            int roll = rollDice(random);
-            positions[i] += roll;
-            System.out.printf("You rolled %d. Your current position is: %s%n", roll, positions[i]);
+        System.out.print("Please enter amount of players: ");
+        int playerCount = scanner.nextInt();
+        scanner.nextLine();
+        int[] positions = new int[playerCount];
 
-            // Actual gameplay
-            boolean isCorrect = getMCInput(question, scanner);
-            positions[i] = getNewPosition(positions[i], isCorrect, specialPos, posInst);
-            System.out.printf("Your new position is: %s. Press ENTER to confirm.%n", positions[i]);
-            scanner.nextLine();  // Lets the player check the results before next player
+        boolean won = false;
+        while (!won) {
+            for (int i = 0; i < positions.length; i++) {
+                System.out.println("Current player: " + (i+1));
+                int roll = rollDice(random);
+                positions[i] += roll;
+                System.out.printf("You rolled %d. Your current position is: %s%n", roll, positions[i]);
+
+                // Actual gameplay
+                boolean isCorrect = getMCInput(getRandomQuestion(random), scanner);
+                positions[i] = getNewPosition(positions[i], isCorrect, specialPos, posInst);
+                System.out.printf("Your new position is: %s. Press ENTER to confirm.%n", positions[i]);
+                scanner.nextLine();  // Lets the player check the results before next player
+                System.out.println("====================");
+                if (!won) won = positions[i] >= 10;
+            }
+            System.out.print("Current leaderboard: ");
+            for (int p : sortedPositions(positions)) System.out.print(p + " ");
+            System.out.println();
+            System.out.println("Press ENTER to confirm.");
+            scanner.nextLine();
             System.out.println("====================");
         }
+        // Current player's position is at least 10, he finished the game.
+        System.out.println("You won!");
+        scanner.nextLine();
     }
 
     // Asks question and returns whether the answer is correct
